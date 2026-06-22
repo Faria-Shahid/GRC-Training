fd1ddeb119b91d293d8f0c44355849e099ab08d9
Faria Shahid <fariya5530@gmail.com>
2026-06-22 16:28:04 +0500

    .

--- Session2/Program.cs
+++ Session2/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IBookService, BookService>();  // ← add this
+builder.Services.AddScoped<IBookService, BookService>();  
 
 var app = builder.Build();
 
